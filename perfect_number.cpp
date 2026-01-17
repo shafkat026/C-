52d748d2d5ef48ea0e7ee6b7d993569557f70f2a
Md Shafkat Sadik <mdshafkatsadik@gmail.com>
2026-01-18 00:59:50 +0600

    Update perfect_number.cpp
    A perfect number is a positive integer that is equal to the sum of its proper divisors, excluding itself. For example, 6 is a perfect number because its divisors (1, 2, and 3) add up to 6.

--- perfect_number.cpp
+++ perfect_number.cpp
@@ -1,3 +1,9 @@
+//A perfect number is a positive integer 
+//that is equal to the sum of its proper divisors, excluding itself. 
+//For example, 6 is a perfect number 
+//because its divisors (1, 2, and 3) add up to 6.
+
+
 #include <iostream>
 using namespace std;
 
